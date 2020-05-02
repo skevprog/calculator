1a4ce63d968b737c90680c78ea4c87c8ac1a351b
skevprog <kevin-s93@hotmail.com>
2020-05-02 18:05:03 +0800

    Mockup

--- src/App.js
+++ src/App.js
@@ -3,7 +3,39 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App" />
+    <div className="App">
+      <div className="calculator">
+        <div className="screen">
+          <span className="result">Screen</span>
+        </div>
+        <div className="keyboard">
+          <div className="row">
+            <div className="key">7</div>
+            <div className="key">8</div>
+            <div className="key">9</div>
+            <div className="key">X</div>
+          </div>
+          <div className="row">
+            <div className="key">4</div>
+            <div className="key">5</div>
+            <div className="key">6</div>
+            <div className="key">-</div>
+          </div>
+          <div className="row">
+            <div className="key">1</div>
+            <div className="key">2</div>
+            <div className="key">3</div>
+            <div className="key">+</div>
+          </div>
+          <div className="row">
+            <div className="key">0</div>
+            <div className="key">.</div>
+            <div className="key">%</div>
+            <div className="key">=</div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
