6be64cdbe10fe61350e565e11169f09e9abe4617
skevprog <kevin-s93@hotmail.com>
2020-05-06 21:35:47 +0800

    Fix equal function bug, refactor code

--- src/utils/index.js
+++ src/utils/index.js
@@ -6,10 +6,17 @@ const divide = (a, b) => a / b;
 
 const multiply = (a, b) => a * b;
 
+const parseToNumber = (value) => {
+  if (typeof value === 'string') {
+    return value.includes('.') ? parseFloat(value) : parseInt(value, 0);
+  }
+  return value;
+};
+
 const calculation = (operator, a, b) => {
   if (!operator) return '';
-  const valueOne = a.includes('.') ? parseFloat(a) : parseInt(a, 0);
-  const valueTwo = b.includes('.') ? parseFloat(b) : parseInt(b, 0);
+  const valueOne = parseToNumber(a);
+  const valueTwo = parseToNumber(b);
   // Avoid executing every operation using common object literal approach
   const Operations = {
     '*': () => multiply(valueOne, valueTwo),
